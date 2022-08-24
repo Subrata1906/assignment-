4c3b26ead584b36f3b69094ec8e9f87a9ec76037
Subrata1906 <110475131+Subrata1906@users.noreply.github.com>
2022-08-24 19:17:13 +0530

    Add files via upload

---
+++ positivenegetive.java
@@ -0,0 +1,19 @@
+//WAJP to get a number from the user and print whether it is positive or negetive
+package Javaassignment;
+import java.util.Scanner;
+
+public class positivenegetive {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Input value of x :");
+		int x= sc.nextInt();
+		if (x<0){
+		System.out.println("Negetive");
+		}else{
+		System.out.println("Positive");
+		}}}
+
+	
+
+
